3af27319365c43c27c5508fc4c9bbc7c587e9c57
Long <long.nguyen@ntq-solution.com.vn>
2017-10-06 09:21:11 +0700

    Fix loi import 1 directive vao module

--- src/app/testmodule/test.component.ts
+++ src/app/testmodule/test.component.ts
@@ -11,7 +11,7 @@ import { TestService } from './test.service';
 
 export class TestComponent implements OnInit {
 	keyCard: string;
-	test = 'abc';
+	test = /^123/i;
 	constructor(private testService: TestService ) {}
 
 	ngOnInit() {
