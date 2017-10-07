bf3ce6c6fee48417e5a8cb1c1b3585671079fbe8
Long <long.nguyen.intern@ntq-solution.com.vn>
2017-10-07 21:12:44 +0700

    azzzz

--- src/app/testmodule/test.module.ts
+++ src/app/testmodule/test.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'
 
 import { TestComponent } from './test.component';
 import { TestService } from './test.service';
 import { UserEmailComponent } from './../user/user-email.component';
+import { TimeFormatPipe } from './../pipes/time-format.pipe'
 
 import { ValidateEmailDirective } from './../directives/validate-email.directive';
 
 @NgModule({
-	imports: [CommonModule],
+	imports: [CommonModule, FormsModule],
 	declarations: [
 		TestComponent,
 		UserEmailComponent,
-		ValidateEmailDirective
+		ValidateEmailDirective,
+		TimeFormatPipe
 		],
 	providers: [TestService],
 	exports: [TestComponent, UserEmailComponent]
